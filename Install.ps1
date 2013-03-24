57e1213c8971508664a11bd7d92b550ab0d99bb0
Marchenko Alexandr <marchenko.alexandr@gmail.com>
2013-03-24 14:27:56 +0200

    install and uninstall initial scripts

---
+++ Install.ps1
@@ -0,0 +1,11 @@
+﻿# [Define HKCR provider](http://fortheloveofcode.wordpress.com/2008/06/08/what-no-hkcr-in-powershell/)
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+
+New-Item -Path HKCR:\Directory\shell -Name AddToSystemPath –Force
+New-ItemProperty HKCR:\Directory\shell\AddToSystemPath -Name "MUIVerb" -Value "Add to system Path"
+New-ItemProperty HKCR:\Directory\shell\AddToSystemPath -Name "Position" -Value "bottom"
+
+New-Item -Path HKCR:\Directory\shell\AddToSystemPath -Name command –Force
+Set-Item -Path HKCR:\Directory\shell\AddToSystemPath\command -Value "cmd.exe /s /k pushd `"%V`""
+
+Copy-Item -Path HKCR:\Directory\shell\AddToSystemPath -Destination HKCR:\Directory\Background\shell -Recurse
---
+++ Uninstall.ps1
@@ -0,0 +1,5 @@
+﻿# [Define HKCR provider](http://fortheloveofcode.wordpress.com/2008/06/08/what-no-hkcr-in-powershell/)
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+
+Remove-Item -Path HKCR:\Directory\shell\AddToSystemPath -Recurse
+Remove-Item -Path HKCR:\Directory\Background\shell\AddToSystemPath -Recurse
